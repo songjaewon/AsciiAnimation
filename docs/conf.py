f616e7f9a0d8570757e53c9902c8db320de1bb55
Jaewon Song <songjaewon@kaist.ac.kr>
2015-06-03 15:55:11 +0900

    0603 Asciimation commit #14.1 - Almost similar

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,7 @@ import shlex
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('C:/Users/cimple/Documents/GitHub/AsciiAnimation'))
+sys.path.insert(0, os.path.abspath('C:/Users/cimple/Documents/GitHub/AsciiAnimation/DocuTest'))
 
 # -- General configuration ------------------------------------------------
 
