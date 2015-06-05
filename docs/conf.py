4194e84f6d56b6e715b3018a32bffcb074f00649
Jaewon Song <songjaewon@kaist.ac.kr>
2015-06-05 17:34:43 +0900

    0605 Asciimation commit #1.4
    - conf.py edited
    -- Mock module added

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return Mock()
 
-MOCK_MODULES = ['freetype', 'cv2']
+MOCK_MODULES = ['freetype', 'cv2', 'numpy', 'matplotlib']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
